d8e0a772374569b42b8822e8e44242a439634806
Will B <wberman27@gmail.com>
2021-03-18 14:07:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {ProductContext} from './contexts/productsContext'
 
 // Components
 import Navigation from './components/Navigation';
